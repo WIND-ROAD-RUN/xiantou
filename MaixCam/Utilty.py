7f1375b887132a1a46df7bd9bd0e51d40f394eaf
rw <rw@DESKTOP-C9MFSUA>
2025-12-15 13:11:17 +0800

    修改模型路径

--- MaixCam/Utilty.py
+++ MaixCam/Utilty.py
@@ -2,7 +2,7 @@ class UtiltyPath:
     def __init__(self):
         self.config_path="/root/workplace/canjian/config.json"
         self.img_path="/root/workplace/canjian/img/"
-        self.model_path="/root/workplace/canjian/model_239252.mud"
+        self.model_path="/root/workplace/canjian/model_245664.mud"
         self.pin_name="A29"
         self.gpio_name="GPIOA29"
 
